5b2023d50d762c2ae91921e9ca326aa752fa3210
mityok69 <ptdimitry@gmail.com>
2013-01-18 20:15:22 +0200

    updated

---
+++ src/com/example/myfirstapp/ListViewActivity.java
@@ -0,0 +1,11 @@
+package com.example.myfirstapp;
+import android.app.Activity;
+import android.os.*;
+public class ListViewActivity extends Activity
+{
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
